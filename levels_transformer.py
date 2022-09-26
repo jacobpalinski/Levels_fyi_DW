3c3ae97151776b0b718acca2f40166712ca2eb2e
JP6780 <kpalinski99@gmail.com>
2022-09-26 19:15:50 +0800

    replace web development

--- levels_transformer.py
+++ levels_transformer.py
@@ -34,15 +34,17 @@ class Levels_ETL:
         '''Transform timestamp from job_data.csv into date_csv with date_key, year, month and quarter'''
         csv_jobdata=self.s3_bucket._bucket.Object(key).get().get('Body').read.decode('UTF-8')
         job_data_df=pd.read_csv(StringIO(csv_jobdata))
-        #Extract date from timestamp
+        # Extract date from timestamp
         date_df=job_data_df['timestamp']
-        date_df['Date']=pd.to_datetime(date_df['Date'],format='%y%m%d')
-        #Extract year from date
+        date_df['Date']=pd.to_datetime(date_df['timestamp'],format='%y%m%d')
+        # Extract year from date
         date_df['Year']=date_df['Date'].dt.year
-        #Extract month from date
+        # Extract month from date
         date_df['Month']=date_df['Date'].dt.month
-        #Exrtact quarter from date
+        # Extract quarter from date
         date_df['Quarter']=date_df['Date'].dt.quarter
+        # Drop timestamp since longer needed
+        date_df.drop('timestamp',axis=1,inplace=True)
         out_buffer=StringIO()
         date_df.to_csv(out_buffer,index=False)
         self.s3_bucket._bucket.put_object(Body=out_buffer.getvalue(),Key='date.csv')
@@ -52,14 +54,39 @@ class Levels_ETL:
         csv_jobdata=self.s3_bucket._bucket.Object(key).get().get('Body').read.decode('UTF-8')
         job_data_df=pd.read_csv(StringIO(csv_jobdata))
         job_details_df=job_data_df[['company','title','tag','level']]
-        # Rename columns
+        # Dealing with empty values
+        job_details_df['company','title','tag','level']=job_details_df[['company','title','tag','level']].replace(
+        r'^\s*$','Not Specified',regex=True)
+        # Rename Columns
         job_details_df.rename(columns={'company':'Company','title':'Title','tag':'Specialisation','level':'Level'})
-        # Dealing with null company names
-        job_details_df['Company']=job_details_df['Company'].replace('','Not Specified')
-        # Dealing with null specialisations
-        job_details_df['Specialisation']=job_details_df['Specialisation'].replace('','Not Specified')
-        # Dealing with null levels
-        job_details_df['']
+        # Replace 'iOS','Android' and Mobile(iOS + Android) specialisation with 'Mobile Development'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('iOS',case=False),
+        'Specialisation']='Mobile Development'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('Android',case=False),
+        'Specialisation']='Mobile Development'
+        # Replace all variations containing 'site reliability' into 'Site Reliability'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('site reliability',case=False),
+        'Specialisation']='Site Reliability'
+        # One label for all production engineers
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('production',case=False),
+        'Specialisation']='Production'
+        # Replace 'fullstack' with 'Full Stack'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('fullstack',case=False),
+        'Specialisation']='Full Stack'
+        # Replace 'augmented reality' and 'virtual reality' with 'AR/VR'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('augmented reality',case=False),
+        'Specialisation']='AR/VR'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('virtual reality',case=False),
+        'Specialisation']='AR/VR'
+        # Replace 'Web Development (front-end) with 'Front End Development'
+        # Replace 'user experience' and 'user interface' with 'UX/UI'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('user experience',case=False),
+        'Specialisation']='UX/UI'
+        job_details_df.loc[job_details_df['Specialisation'].str.contains('user interface',case=False),
+        'Specialisation']='UX/UI'
+
+
+
 
 
 
